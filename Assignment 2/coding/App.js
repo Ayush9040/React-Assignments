6f2211af72b49fa02ecba9d7a1e8881a8403cde0
unknown <ayushaggarwal9040@gmail.com>
2023-02-09 18:38:15 +0530

    coding react

---
+++ Assignment 2/coding/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+
+
+const heading1 = React.createElement("h1",{className: "main"}, {key: "main"} , "heading 1");
+
+const heading2 = React.createElement("h2",{className: "sub", style: 'text-align: center' }, "heading 2");
+
+const container = React.createElement("div", {id: "container"}, [heading1, heading2]);
+
+console.log(container);
+const home = document.getElementById("root");
+
+
+const ok = ReactDOM.createRoot(home);
+
+ok.render(container);
+
